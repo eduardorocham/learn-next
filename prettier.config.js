0df6aa3163ffd1626e9b3ace489fe5c8b407f7cd
eduardo <jose.eduardo@inbec.edu.br>
2024-04-10 11:58:23 -0300

    chapter 7

--- prettier.config.js
+++ prettier.config.js
@@ -3,4 +3,8 @@ const styleguide = require('@vercel/style-guide/prettier');
 module.exports = {
   ...styleguide,
   plugins: [...styleguide.plugins, 'prettier-plugin-tailwindcss'],
+  singleQuote: true,
+  semi: false,
+  tabWidth: 2,
+  bracketSpacing: true
 };
